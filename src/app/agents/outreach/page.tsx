1018544eebbcab579d3fe0739e495ece71628183
Samarth Kapoor <kapoorsamarth7@gmail.com>
2025-01-07 22:44:59 +0530

    outreach page

---
+++ src/app/agents/outreach/page.tsx
@@ -0,0 +1,10 @@
+import { BotSettings } from '@/components/bot/settings'
+
+export default function OutreachBotPage() {
+  return (
+    <div className="flex-1 space-y-8 p-8 pt-6">
+      <BotSettings />
+    </div>
+  )
+}
+
